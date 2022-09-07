8568d5015a6eab97f528631ba1dd785eed92b547
XUEYI.XIA <xueyi.xia@cn.aastocks.com>
2022-09-07 09:59:42 +0800

    增加远程依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+        maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev") }
     }
 }
 
