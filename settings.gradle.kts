d0d3b664eef1bd5b2e74dfbe42580eecc9462955
XUEYI.XIA <xtw007@126.com>
2022-09-08 17:34:29 +0800

    更新build 配置文件

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,20 @@
-pluginManagement {
-    repositories {
-        google()
-        gradlePluginPortal()
-        mavenCentral()
-    }
+//pluginManagement {
+//    repositories {
+//        google()
+//        gradlePluginPortal()
+//        mavenCentral()
+//    }
+//}
+
+plugins {
+    //   https://github.com/alexvasilkov/GradleGitDependenciesPlugin
+   id("com.alexvasilkov.git-dependencies") version "2.0.4"
 }
+
 
 rootProject.name = "As_KmmApp"
 include(":androidApp")
-include(":shared")
+//include(":shared")
 
 include(":library")
+include(":KmmSharedLibrary")
