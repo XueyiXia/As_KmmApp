9c5947bcccb81b3a273c483f1c8e7f3c9b925fd5
xtw007@outlook.com <xtw719780103+>
2022-09-13 13:32:42 +0800

    删除请求url ，采用公共的URL

--- KmmSharedLibrary/src/commonMain/kotlin/com/kmm/app/api/HttpApi.kt
+++ KmmSharedLibrary/src/commonMain/kotlin/com/kmm/app/api/HttpApi.kt
@@ -9,7 +9,6 @@ package com.kmm.app.api
  */
 object HttpApi {
 
-    const val BASE_URL: String="http://fctdata.aastocks.com:9001/getDaily?id=00700.HK&interval=1m&period=1D";
+    const val BASE_URL="https://blog.jetbrains.com/kotlin/feed/"
 
-    const val test="getDaily"
 }
